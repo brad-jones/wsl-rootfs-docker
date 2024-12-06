fdd3a3bdc151a60bf09d83ab1c07f3484aed9142
Brad Jones <brad@bjc.id.au>
2024-12-06 16:56:17 +1100

    chore: lets use deno to build pipeline

---
+++ main.ts
@@ -0,0 +1,16 @@
+#!/usr/bin/env -S deno run -A
+import $ from "@david/dax";
+import { z } from "zod";
+
+const latestTag = z.object({ Tags: z.array(z.string()) })
+  .parse(await $`skopeo list-tags docker://docker.io/docker`.json())
+  .Tags.filter((_) => _.match(/^\d+\.\d+\.\d+-dind-alpine.*$/))
+  .reverse()[0];
+
+$.log(`Latest version from registry: ${latestTag}`);
+
+/*
+- run: docker export $(docker create docker:latest) | gzip > rootfs.tar.gz
+      - run: tar -tf rootfs.tar.gz
+      - run: sha256sum -b rootfs.tar.gz
+*/
